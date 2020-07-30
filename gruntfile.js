4835a9e3e91c0a16d1e6ab0331be26094d0fe903
Filipe <filipedasilvadeoliveira@gmail.com>
2020-07-30 13:50:17 -0300

    feature: new typedoc tago theme

--- gruntfile.js
+++ gruntfile.js
@@ -27,33 +27,6 @@ module.exports = function(grunt)
                 }
             }
         },
-        'string-replace': {
-            themeMinimal: {
-                files: {
-                    'bin/minimal/layouts/default.hbs': ['src/minimal/layouts/default.hbs']
-                },
-                options: {
-                    replacements: [{
-                        pattern: /{{ CSS }}/g,
-                        replacement: function() {
-                            var css = grunt.file.read('bin/default/assets/css/main.css');
-                            return css.replace(/url\(([^\)]*)\)/g, function(match, file) {
-                                if (match.indexOf(':') != -1) return match;
-                                var path = require('path'), fs = require('fs');
-                                var file = path.resolve('bin/default/assets/css', file);
-                                var data = fs.readFileSync(file, 'base64');
-                                return 'url(data:image/png;base64,' + data + ')';
-                            });
-                        }
-                    }, {
-                        pattern: /{{ JS }}/g,
-                        replacement: function() {
-                            return grunt.file.read('bin/default/assets/js/main.js').replace('{{', '{/**/{');
-                        }
-                    }]
-                }
-            }
-        },
         sass: {
             options: {
                 implementation: sass,
@@ -95,24 +68,13 @@ module.exports = function(grunt)
                     cwd: 'src/default',
                     src: ['**/*.hbs', '**/*.png'],
                     dest: 'bin/default'
+                }, {
+                    expand: true,
+                    cwd: 'src/default/assets/tago',
+                    src: ['**/*'],
+                    dest: 'bin/default/assets/tago'
                 }]
             },
-            themeDefault2Minimal: {
-                files: [{
-                    expand: true,
-                    cwd: 'src/default/partials',
-                    src: ['**/*.hbs'],
-                    dest: 'bin/minimal/partials'
-                }]
-            },
-            themeMinimal: {
-                files: [{
-                    expand: true,
-                    cwd: 'src/minimal',
-                    src: ['**/*.hbs'],
-                    dest: 'bin/minimal'
-                }]
-            }
         },
         watch: {
             js: {
@@ -125,16 +87,8 @@ module.exports = function(grunt)
             },
             default: {
                 files: ['src/default/**/*.hbs'],
-                tasks: ['copy', 'string-replace']
+                tasks: ['copy']
             },
-            minimal: {
-                files: ['src/minimal/partials/**/*.hbs', 'src/minimal/templates/**/*.hbs'],
-                tasks: ['copy:themeMinimal']
-            },
-            minimalLayout: {
-                files: ['src/minimal/layouts/default.hbs'],
-                tasks: ['string-replace:themeMinimal']
-            }
         }
     });
 
@@ -143,11 +97,10 @@ module.exports = function(grunt)
     grunt.loadNpmTasks('grunt-sass');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-string-replace');
     grunt.loadNpmTasks('grunt-autoprefixer');
     grunt.loadNpmTasks('grunt-ts');
 
     grunt.registerTask('css', ['sass', 'autoprefixer']);
     grunt.registerTask('js', ['ts:themeDefault', 'uglify']);
-    grunt.registerTask('default', ['copy', 'css', 'js', 'string-replace']);
+    grunt.registerTask('default', ['copy', 'css', 'js']);
 };
